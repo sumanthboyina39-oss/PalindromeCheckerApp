326e37399fe8a42a0d8f1cf948c0c5257e607034
sumanthboyina39-oss <sumanthboyina39@gmail,com>
2026-03-06 15:16:24 +0530

    UC9-exp

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,30 +1,20 @@
-import java.util.LinkedList;
-import java.util.Stack;
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String word = "level";
-        boolean isPalindrome = true;
-
-        LinkedList<Character> list = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : word.toCharArray()) {
-            list.add(c);
-            stack.push(c);
-        }
-
-        while (!list.isEmpty()) {
-            if (!list.removeFirst().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
+        if (isPalindrome(word, 0, word.length() - 1)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
         }
+    }
+
+    public static boolean isPalindrome(String word, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(word, start + 1, end - 1);
     }
 }
